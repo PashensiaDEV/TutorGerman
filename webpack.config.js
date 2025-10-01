ee39edcb623685e732dbca996bf1294c0c2fa4ad
Pasha Lobach <Pashensia@gmail.com>
2025-10-01 19:04:01 +0200

    updates

--- webpack.config.js
+++ webpack.config.js
@@ -1,65 +1,96 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+// Generated using webpack-cli https://github.com/webpack/webpack-cli
 
-module.exports = {
-  entry: {
-    main: './src/scripts/index.js'
-  },
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const { DefinePlugin } = require('webpack');
+const TerserPlugin = require("terser-webpack-plugin");
+
+require('dotenv').config({
+  path: path.join(process.cwd(), process.env.NODE_ENV ? `.env.${process.env.NODE_ENV}` : '.env')
+});
+
+const isProduction = process.env.NODE_ENV == "production";
+
+const stylesHandler = MiniCssExtractPlugin.loader;
+
+const config = {
+  entry: "./src/index.ts",
+  devtool: "source-map",
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'main.js',
-    publicPath: '',
+    path: path.resolve(__dirname, "dist"),
   },
-  mode: 'development',
   devServer: {
-    static: path.resolve(__dirname, './dist'),
     open: true,
-    compress: true,
-    port: 8080
-  },
-  module: {
-    rules: [{
-        test: /\.js$/,
-        use: {
-          loader: 'babel-loader'
-        },
-        exclude: '/node_modules/'
-      },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/,
-        type: 'asset/resource',
-        generator: {
-            filename: 'images/[name].[hash][ext]',
-        }
-    },
-    {
-      test: /\.(woff|woff2|eot|ttf|otf)$/i,
-      type: 'asset/resource',
-      generator: {
-        filename: 'fonts/[name].[hash][ext]',
-      }
-    }, 
-      {
-        test: /\.css$/,
-        use: [MiniCssExtractPlugin.loader, {
-            loader: 'css-loader',
-            options: {
-              importLoaders: 1
-            }
-          },
-          'postcss-loader'
-        ]
-      },
-    ]
+    host: "localhost",
+    watchFiles: ["src/pages/*.html"],
+    hot: true
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: './src/index.html'
+      template: "src/pages/index.html"
     }),
-    new CleanWebpackPlugin(),
+
     new MiniCssExtractPlugin(),
 
-  ]
-}
+    // Add your plugins here
+    // Learn more about plugins from https://webpack.js.org/configuration/plugins/
+    new DefinePlugin({
+      'process.env.DEVELOPMENT': !isProduction,
+      'process.env.API_ORIGIN': JSON.stringify(process.env.API_ORIGIN ?? '')
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/i,
+        use: ["babel-loader", "ts-loader"],
+        exclude: ["/node_modules/"],
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [stylesHandler, "css-loader", "postcss-loader", "resolve-url-loader", {
+          loader: "sass-loader",
+          options: {
+            sourceMap: true,
+            sassOptions: {
+              includePaths: ["src/scss"]
+            }
+          }
+        }],
+      },
+      {
+        test: /\.css$/i,
+        use: [stylesHandler, "css-loader", "postcss-loader"],
+      },
+      {
+        test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
+        type: "asset",
+      },
+
+      // Add your rules for custom modules here
+      // Learn more about loaders from https://webpack.js.org/loaders/
+    ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".jsx", ".js", "..."],
+  },
+  optimization: {
+    minimize: true,
+    minimizer: [new TerserPlugin({
+      terserOptions: {
+        keep_classnames: true,
+        keep_fnames: true
+      }
+    })]
+  }
+};
+
+module.exports = () => {
+  if (isProduction) {
+    config.mode = "production";
+  } else {
+    config.mode = "development";
+  }
+  return config;
+};
